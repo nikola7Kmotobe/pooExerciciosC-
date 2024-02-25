cd99177c7ff60e780cc3a292a207c9c302f4749e
nikola7Kmotobe <nikolamotobe@gmail.com>
2024-02-25 14:38:49 -0300

    BichinhoVirtual

---
+++ BichinhoVirtual/Bichinho.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BichinhoVirtual
+{
+    internal class Bichinho
+    {
+        public string Nome { get; set; }
+        public string Classe { get; set; }
+        public string Familia { get; set; }
+        public int Idade { get; set; }
+        public bool Estado { get; set; }
+        public int Caloria { get; set; }
+        public int Forca { get; set; }
+
+        public Bichinho(string nome, string classe, string familia, int idade, bool estado, int caloria, int forca)
+        {
+            Nome = nome;
+            Classe = classe;
+            Familia = familia;
+            Idade = idade;
+            Estado = estado;
+            Caloria = caloria;
+            Forca = forca;
+        }
+
+        public Bichinho(string nome, string classe, string familia)
+        {
+            Nome = nome;
+            Classe = classe;
+            Familia = familia;
+            Idade = 0;
+            Estado = true;
+            Caloria = 10;
+            Forca = 10;
+        }
+
+        public string Nasceu() {
+            return $"O animal se chama {Nome} é da classe {Classe}, da familia {Familia}, o animal possuí força de {Forca}, caloria de {Caloria} e idade {Idade}";
+        }
+
+        public string Morreu()
+        {
+            Estado = false;
+            Forca = 0;
+            return $"O bixinho morreu";
+        }
+
+        public string Comeu()
+        {
+            Caloria += 10;
+            Forca -= 2;
+            return $"O bixinho está com caloria {Caloria} e força {Forca}";
+        }
+
+        public string Correr()
+        {
+            Caloria -= 5;
+            Forca -= 5;
+            return $"O bixinho está correndo! Agora está com caloria {Caloria} e força {Forca}";
+        }
+
+        public string Dormir()
+        {
+            Caloria -= 2;
+            Forca += 10;
+            return $"O animal está domingo agora! Agora está com caloria {Caloria} e força {Forca}";
+        }
+    }
+}
---
+++ BichinhoVirtual/Program.cs
@@ -0,0 +1,52 @@
+﻿using BichinhoVirtual;
+using static System.Net.Mime.MediaTypeNames;
+
+int cont = 0;
+try
+{
+    Console.Write("Crie o seu novo Bichinho \n Insira o nome: ");
+    string nome = Console.ReadLine()!;
+
+    Console.Write("Insira a classe: ");
+    string classe = Console.ReadLine()!;
+
+    Console.Write("Insira a familia: ");
+    string familia = Console.ReadLine()!;
+
+    Bichinho b1 = new Bichinho(nome, classe, familia);
+    Console.WriteLine(b1.Nasceu());
+
+    while (cont != 4)
+    {
+        Console.WriteLine("O que o " + b1.Nome + " vai fazer agora ? " + "\n1 - Comer \n2 - Correr \n3 - Dormir \n4 - Morrer");
+        cont = int.Parse(Console.ReadLine()!);
+        if (cont > 0 || cont < 5)
+        {
+            switch (cont)
+            {
+                case 1:
+                    Console.WriteLine(b1.Comeu());
+                    break;
+                case 2:
+                    Console.WriteLine(b1.Correr());
+                    break;
+                case 3:
+                    Console.WriteLine(b1.Dormir());
+                    break;
+                case 4:
+                    Console.WriteLine(b1.Morreu());
+                    break;
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("Opção inválida, tente novamente");
+        }
+    }
+    Console.WriteLine("Fim do nosso ciclo, Amigão!");
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
